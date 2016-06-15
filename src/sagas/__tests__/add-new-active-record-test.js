16f259cc4ca97be2c409a9174f8f34a09175bdb1
yuichiroTCY <t.yic.yt@gmail.com>
2016-06-16 01:28:07 +0900

    (refs #2) Modify newly added record id to use unix timestamp instead of random string

--- src/sagas/__tests__/add-new-active-record-test.js
+++ src/sagas/__tests__/add-new-active-record-test.js
@@ -1,30 +1,53 @@
+jest.unmock('redux-saga')
 jest.unmock('redux-saga/effects')
 jest.unmock('../../actions')
 jest.unmock('../add-new-active-record')
 
-import Chance from 'chance'
-import { put } from 'redux-saga/effects'
+import moment from 'moment'
+import { delay } from 'redux-saga'
+import { take, put, call } from 'redux-saga/effects'
 import {
   addActiveRecord,
+  ADD_NEW_ACTIVE_RECORD,
 } from '../../actions'
-import { addNewActiveRecord } from '../add-new-active-record'
+import {
+  addNewActiveRecord,
+  watchAddNewActiveRecord,
+} from '../add-new-active-record'
 
 describe('ADD_NEW_ACTIVE_RECORD', () => {
   it('calls ADD_ACTIVE_RECORD with new ID', () => {
-    const mockAction = {
-      patientId: 'patient_foo',
-    }
-    Chance.prototype.string.mockReturnValue('thisismockedid')
+    const mockMomentFormat = jest.fn().mockReturnValue('mocked-datetime')
+    moment.mockReturnValue({
+      format: mockMomentFormat,
+    })
 
-    const saga = addNewActiveRecord(mockAction)
+    const saga = addNewActiveRecord('patient_foo')
 
     expect(saga.next().value)
       .toEqual(put(addActiveRecord({
-        _id: 'record_foo_thisismockedid',
+        _id: 'record_foo_mocked-datetime',
         type: 'record',
       })))
 
     expect(saga.next())
       .toEqual({done: true, value: undefined})
+
+    expect(mockMomentFormat).toBeCalledWith('x')  // Unix Millisecond Timestamp
+  })
+})
+
+describe('watchAddNewActiveRecord', () => {
+  it('calls addNewActiveRecord() with 1ms delay in blocking way', () => {
+    const saga = watchAddNewActiveRecord()
+
+    expect(saga.next().value)
+      .toEqual(take(ADD_NEW_ACTIVE_RECORD))
+
+    expect(saga.next({patientId: 'patient_bar'}).value)
+      .toEqual(call(addNewActiveRecord, 'patient_bar'))
+
+    expect(saga.next().value)
+      .toEqual(call(delay, 1))
   })
 })
--- src/sagas/add-new-active-record.js
+++ src/sagas/add-new-active-record.js
@@ -1,23 +1,27 @@
-import { takeLatest, takeEvery } from 'redux-saga'
+import { takeLatest, takeEvery, delay } from 'redux-saga'
 import { take, put, call, fork, select } from 'redux-saga/effects'
-import Chance from 'chance'
+import moment from 'moment'
 import {
   ADD_NEW_ACTIVE_RECORD,
   addActiveRecord,
 } from '../actions'
 
-const chance = new Chance()
-const idStringPool = '0123456789abcefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ' // URLセーフな文字のみ
-
-export function* addNewActiveRecord(action) {
-  const patientIdBody = action.patientId.replace(/^patient_/, '')
-  const id = chance.string({pool: idStringPool, length: 16})
+export function* addNewActiveRecord(patientId) {
+  const patientIdBody = patientId.replace(/^patient_/, '')
+  const datetime = moment().format('x') // Unix Millisecond Timestamp
   yield put(addActiveRecord({
-    _id: `record_${patientIdBody}_${id}`,
+    _id: `record_${patientIdBody}_${datetime}`,
     type: 'record',
   }))
 }
 
 export function* watchAddNewActiveRecord() {
-  yield* takeEvery(ADD_NEW_ACTIVE_RECORD, addNewActiveRecord)
+  while (true) {
+    let { patientId } = yield take(ADD_NEW_ACTIVE_RECORD)
+    yield call(addNewActiveRecord, patientId)
+    yield call(delay, 1)
+    // record IDが1msごとに採番されるので1ms待つ
+    // TODO 別のclientで同一patientに1ms以内に同時にrecordを追加されるとconflictする
+    // この辺はしっかり考えるべき
+  }
 }
