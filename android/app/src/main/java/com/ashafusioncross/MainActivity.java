3521be5f337f37eebf57fe82ac9de138a559dc23
yuichiroTCY <t.yic.yt@gmail.com>
2016-06-17 02:34:16 +0900

    (refs #8) Introduce fabric

--- android/app/src/main/java/com/ashafusioncross/MainActivity.java
+++ android/app/src/main/java/com/ashafusioncross/MainActivity.java
@@ -7,7 +7,17 @@ import com.facebook.react.shell.MainReactPackage;
 import java.util.Arrays;
 import java.util.List;
 
+// Crashlytics
+import android.os.Bundle;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
+
 public class MainActivity extends ReactActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      Fabric.with(this, new Crashlytics());
+    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
