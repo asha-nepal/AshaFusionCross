b329f7aa1d348ef20738f6b50baeaf1e5b05e6a3
yuichiroTCY <t.yic.yt@gmail.com>
2016-06-24 21:33:27 +0900

    (refs #24) Modify db/subscribe to use Object to store callbacks

--- src/db/subscribe.js
+++ src/db/subscribe.js
@@ -1,27 +1,33 @@
 /* @flow */
 
-let callbacks = []
+let callbacks
+let nextind
 
 export const subscribe = (cb: Function, key: string) => {
   const entity: {cb: ?Function, key: string} = {
     cb,
     key,
   }
-  callbacks.push(entity)
 
-  return () => {
-    entity.cb = undefined
-  }
+  const ind = nextind
+  callbacks[ind] = entity
+  nextind++
+
+  return () => { delete callbacks[ind] }
 }
 
 export const trigger = (key: string, ...args: Array<void>) => {
-  callbacks.forEach(entity => {
-    if (entity.cb === 'function')
-      if (!key || !entity.key || key === entity.key)
-        entity.cb.apply(null, args)
+  Object.keys(callbacks).forEach(ind => {
+    const entity = callbacks[ind]
+
+    if (!key || !entity.key || key === entity.key)
+      entity.cb.apply(null, args)
   })
 }
 
 export const clear = () => {
-  callbacks = []
+  callbacks = {}
+  nextind = 0
 }
+
+clear()
