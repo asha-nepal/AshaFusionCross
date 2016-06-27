6142683b7b9f2a86401392f90fa8cd4b826f28ff
yuichiroTCY <t.yic.yt@gmail.com>
2016-06-28 00:02:56 +0900

    (refs #24) Rename subscriber to pubsub

--- src/db/__tests__/pubsub-test.js
+++ src/db/__tests__/pubsub-test.js
@@ -1,38 +1,38 @@
 /* @flow */
 
-jest.unmock('../subscriber')
+jest.unmock('../pubsub')
 
-//import { subscribe, trigger, clear } from '../subscribe'
-import Subscriber from '../subscriber'
+//import { subscribe, publish, clear } from '../subscribe'
+import PubSub from '../pubsub'
 
 describe('subscribe()', () => {
-  it('registers callbacks and fire them if trigger() is called', () => {
-    const subscriber = new Subscriber()
+  it('registers callbacks and fire them if publish() is called', () => {
+    const pubsub = new PubSub()
 
     const cb1 = jest.fn()
     const cb2 = jest.fn()
 
-    const unsubscribe1 = subscriber.subscribe(cb1)
-    const unsubscribe2 = subscriber.subscribe(cb2)
+    const unsubscribe1 = pubsub.subscribe(cb1)
+    const unsubscribe2 = pubsub.subscribe(cb2)
 
     expect(cb1).not.toBeCalled()
     expect(cb2).not.toBeCalled()
 
-    subscriber.trigger()
+    pubsub.publish()
 
     expect(cb1).toBeCalled()
     expect(cb2).toBeCalled()
 
     unsubscribe1()
 
-    subscriber.trigger()
+    pubsub.publish()
 
     expect(cb1.mock.calls.length).toBe(1)
     expect(cb2.mock.calls.length).toBe(2)
   })
 
   it('can set keys to specify which callbacks are to be called', () => {
-    const subscriber = new Subscriber()
+    const pubsub = new PubSub()
 
     const cball = jest.fn()
     const cbA1 = jest.fn()
@@ -40,13 +40,13 @@ describe('subscribe()', () => {
     const cbB1 = jest.fn()
     const cbB2 = jest.fn()
 
-    const unsubscribeall = subscriber.subscribe(cball)
-    const unsubscribeA1  = subscriber.subscribe('A', cbA1)
-    const unsubscribeA2  = subscriber.subscribe('A', cbA2)
-    const unsubscribeB1  = subscriber.subscribe('B', cbB1)
-    const unsubscribeB2  = subscriber.subscribe('B', cbB2)
+    const unsubscribeall = pubsub.subscribe(cball)
+    const unsubscribeA1  = pubsub.subscribe('A', cbA1)
+    const unsubscribeA2  = pubsub.subscribe('A', cbA2)
+    const unsubscribeB1  = pubsub.subscribe('B', cbB1)
+    const unsubscribeB2  = pubsub.subscribe('B', cbB2)
 
-    subscriber.trigger('A')
+    pubsub.publish('A')
 
     expect(cball.mock.calls.length).toBe(1)
     expect(cbA1.mock.calls.length).toBe(1)
@@ -54,14 +54,14 @@ describe('subscribe()', () => {
     expect(cbB1.mock.calls.length).toBe(0)
     expect(cbB2.mock.calls.length).toBe(0)
 
-    subscriber.trigger('B')
+    pubsub.publish('B')
     expect(cball.mock.calls.length).toBe(2)
     expect(cbA1.mock.calls.length).toBe(1)
     expect(cbA2.mock.calls.length).toBe(1)
     expect(cbB1.mock.calls.length).toBe(1)
     expect(cbB2.mock.calls.length).toBe(1)
 
-    subscriber.trigger()
+    pubsub.publish()
     expect(cball.mock.calls.length).toBe(3)
     expect(cbA1.mock.calls.length).toBe(2)
     expect(cbA2.mock.calls.length).toBe(2)
@@ -69,7 +69,7 @@ describe('subscribe()', () => {
     expect(cbB2.mock.calls.length).toBe(2)
 
     unsubscribeA1()
-    subscriber.trigger()
+    pubsub.publish()
     expect(cball.mock.calls.length).toBe(4)
     expect(cbA1.mock.calls.length).toBe(2)
     expect(cbA2.mock.calls.length).toBe(3)
@@ -78,25 +78,25 @@ describe('subscribe()', () => {
   })
 })
 
-describe('trigger()', () => {
+describe('publish()', () => {
   it('can be called with arguments', () => {
-    const subscriber = new Subscriber()
+    const pubsub = new PubSub()
 
     const cbA = jest.fn()
     const cball = jest.fn()
 
-    subscriber.subscribe('A', cbA)
-    subscriber.subscribe(cball)
+    pubsub.subscribe('A', cbA)
+    pubsub.subscribe(cball)
 
-    subscriber.trigger(null, 'yoho', 1)
+    pubsub.publish(null, 'yoho', 1)
     expect(cbA).toBeCalledWith('yoho', 1)
     expect(cball).toBeCalledWith('yoho', 1)
 
-    subscriber.trigger('A', 'foo', 100)
+    pubsub.publish('A', 'foo', 100)
     expect(cbA).toBeCalledWith('foo', 100)
     expect(cball).toBeCalledWith('foo', 100)
 
-    subscriber.trigger('B', 'bar', 200)
+    pubsub.publish('B', 'bar', 200)
     expect(cbA).not.toBeCalledWith('bar', 200)
     expect(cball).toBeCalledWith('bar', 200)
   })
@@ -104,24 +104,24 @@ describe('trigger()', () => {
 
 describe('clear()', () => {
   it('deletes all callbacks', () => {
-    const subscriber = new Subscriber()
+    const pubsub = new PubSub()
 
     const cb1 = jest.fn()
     const cb2 = jest.fn()
 
-    subscriber.subscribe(cb1)
-    subscriber.subscribe(cb2)
+    pubsub.subscribe(cb1)
+    pubsub.subscribe(cb2)
 
     expect(cb1).not.toBeCalled()
     expect(cb2).not.toBeCalled()
 
-    subscriber.trigger()
+    pubsub.publish()
     expect(cb1.mock.calls.length).toBe(1)
     expect(cb2.mock.calls.length).toBe(1)
 
-    subscriber.clear()
+    pubsub.clear()
 
-    subscriber.trigger()
+    pubsub.publish()
     expect(cb1.mock.calls.length).toBe(1)
     expect(cb2.mock.calls.length).toBe(1)
   })
