85d58bc522b7edf5990810a0ac6b425af20fb570
yuichiroTCY <t.yic.yt@gmail.com>
2016-07-01 00:26:37 +0900

    (refs #27) Install react-native-vector-icons

--- android/app/src/main/java/com/ashafusioncross/MainActivity.java
+++ android/app/src/main/java/com/ashafusioncross/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ashafusioncross;
 
 import com.facebook.react.ReactActivity;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.github.xinthink.rnmk.ReactMaterialKitPackage;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -46,6 +47,7 @@ public class MainActivity extends ReactActivity {
     protected List<ReactPackage> getPackages() {
         return Arrays.<ReactPackage>asList(
             new MainReactPackage(),
+            new VectorIconsPackage(),
             new ReactMaterialKitPackage()
         );
     }
