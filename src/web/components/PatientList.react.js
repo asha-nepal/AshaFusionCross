d592dd29827d52c0d96f6bbe7e27b0d5d472d478
yuichiroTCY <t.yic.yt@gmail.com>
2016-07-21 00:02:03 +0900

    (refs #32) Modify PatientList design

--- src/web/components/PatientList.react.js
+++ src/web/components/PatientList.react.js
@@ -43,22 +43,19 @@ export default class PatientList extends Component {
       <div>
         <section className="section">
           <div className="container">
-            <Link to={'/patient/'} className="button">
-              New
-            </Link>
-          </div>
-        </section>
-
-        <section className="section">
-          <div className="container">
             <nav className="panel">
-            {patientList.map((patient) =>
-              <Link
-                key={patient._id}
-                className="panel-block"
-                to={`/patient/${patient._id}`}
-              >{patient.name || ''}</Link>
-            )}
+              <div className="panel-block">
+                <Link to={'/patient/'} className="button is-primary is-outlined is-fullwidth">
+                  New
+                </Link>
+              </div>
+              {patientList.map((patient) =>
+                <Link
+                  key={patient._id}
+                  className="panel-block"
+                  to={`/patient/${patient._id}`}
+                >{patient.name || ''}</Link>
+              )}
             </nav>
           </div>
         </section>
