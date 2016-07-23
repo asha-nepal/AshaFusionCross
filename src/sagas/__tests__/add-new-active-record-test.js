0776a20c18a7d5fec6da73b72377498c3bf4dbfb
yuichiroTCY <t.yic.yt@gmail.com>
2016-07-24 03:11:35 +0900

    (refs #46) Modify test of ADD_NEW_ACTIVE_RECORD to use react-redux-form and its dependency redux-thunk

--- src/sagas/__tests__/add-new-active-record-test.js
+++ src/sagas/__tests__/add-new-active-record-test.js
@@ -1,15 +1,17 @@
-/* eslint-env jest */
+/* eslint-env jest, jasmine */
 
+jest.unmock('redux-thunk');
 jest.unmock('redux-saga');
 jest.unmock('redux-saga/effects');
 jest.unmock('react-redux-form');
 jest.unmock('../../actions');
 jest.unmock('../add-new-active-record');
+jest.unmock('underscore');
 
 import moment from 'moment';
 import { delay } from 'redux-saga';
 import { take, put, call } from 'redux-saga/effects';
-import { actions as formActions } from 'react-redux-form';
+import { actionTypes as formActionTypes } from 'react-redux-form';
 import {
   ADD_NEW_ACTIVE_RECORD,
   selectActiveRecord,
@@ -28,11 +30,35 @@ describe('ADD_NEW_ACTIVE_RECORD', () => {
 
     const saga = addNewActiveRecord('patient_foo');
 
-    expect(saga.next().value)
+//    expect(saga.next().value)
 //      .toEqual(put(formActions.push('activeRecords', {  // TODO: thunk actionの比較
 //        _id: 'record_foo_mocked-datetime',
 //        type: 'record',
 //      })));
+// 本来は上記のようなテストを短く書きたいが，redux-thunk actionの取り扱いに手間取り，以下の様なテストになった
+    const value1 = saga.next().value;
+    const action1 = value1.PUT.action;
+    expect(action1).toEqual(jasmine.any(Function));
+
+    const dummyDispatch = jest.fn();
+    const dummyGetState = jest.fn().mockReturnValue({
+      activeRecords: [
+        'existing data',
+      ],
+    });
+    action1(dummyDispatch, dummyGetState);
+    expect(dummyDispatch).toBeCalledWith({
+      type: formActionTypes.CHANGE,
+      model: 'activeRecords',
+      value: [
+        'existing data',
+        {
+          _id: 'record_foo_mocked-datetime',
+          type: 'record',
+        },
+      ],
+    });
+
 
     expect(saga.next().value)
       .toEqual(put(selectActiveRecord('record_foo_mocked-datetime')));
