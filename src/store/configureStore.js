e610be1dab90f0bf60f43677391779177035bcfc
yuichiroTCY <t.yic.yt@gmail.com>
2016-08-14 01:35:13 +0900

    (refs #92) Introduce redux-dev-tool-extension

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,7 @@
 import {
   createStore,
   applyMiddleware,
+  compose,
 } from 'redux';
 import thunk from 'redux-thunk';
 import createSagaMiddleware from 'redux-saga';
@@ -9,8 +10,19 @@ import reducers from './reducers';
 
 export default () => {
   const sagaMiddleware = createSagaMiddleware();
+  const middlewares = applyMiddleware(sagaMiddleware, thunk);
   return {
-    ...createStore(reducers, applyMiddleware(sagaMiddleware, thunk)),
+    ...createStore(
+      reducers,
+      process.env.NODE_ENV
+        ? middlewares
+        : compose(
+          middlewares,
+          typeof window === 'object' && typeof window.devToolsExtension !== 'undefined'
+            ? window.devToolsExtension()
+            : f => f
+        )
+    ),
     runSaga: sagaMiddleware.run,
   };
 };
