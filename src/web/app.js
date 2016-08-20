8a010400d5ef30887a252cb468f5f8f8454ca7d9
yuichiroTCY <t.yic.yt@gmail.com>
2016-08-20 23:41:49 +0900

    (refs #139) Introduce react-addons-perf

--- src/web/app.js
+++ src/web/app.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App.react';
 
+if (process.env.NODE_ENV !== 'production') {
+  window.Perf = require('react-addons-perf'); // eslint-disable-line global-require
+}
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
