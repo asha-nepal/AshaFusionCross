ec13628c442072a46f781d8393e99960cf40a7c7
yuichiroTCY <t.yic.yt@gmail.com>
2016-08-30 06:03:50 +0700

    (refs #103) Add readonly prop to TextArea

--- src/web/forms/fields/TextArea.js
+++ src/web/forms/fields/TextArea.js
@@ -1,18 +1,52 @@
 /* @flow */
 
 import React from 'react';
-import { Field } from 'react-redux-form';
+import { connect } from 'react-redux';
+import { actions } from 'react-redux-form';
+import _get from 'lodash.get';
 
-export const TextArea = ({
-  model,
+export const TextAreaComponent = ({
+  value,
   label,
   style,
   placeholder,
-}: TextAreaProps) => (
+  readonly = false,
+  onChange,
+}: {
+  value: ?string,
+  label: ?string,
+  style: ?Object,
+  placeholder: ?string,
+  readonly: boolean,
+  onChange: (newValue: string) => void,
+}) => (
   <p className="control" style={style}>
     {label && <label className="label">{label}</label>}
-    <Field model={model}>
-      <textarea className="textarea" placeholder={placeholder} />
-    </Field>
+    {readonly ? (
+      <span className="form-static is-multiline">
+        {value && value.split('\n').reduce(
+          (a, b, i) => a.concat([b, <br key={i} />]), []
+        )}
+      </span>
+    ) : (
+      <textarea
+        className="textarea"
+        placeholder={placeholder}
+        value={value}
+        onChange={e => onChange(e.target.value)}
+      />
+    )}
   </p>
 );
+
+const mapStateToProps = (state, ownProps) => ({
+  value: _get(state, ownProps.model),
+});
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  onChange: (newValue) => dispatch(actions.change(ownProps.model, newValue)),
+});
+
+export const TextArea = connect(
+  mapStateToProps, mapDispatchToProps
+)(TextAreaComponent);
