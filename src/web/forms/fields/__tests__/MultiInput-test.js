5420da2ff7280549d2cb0457b8a3f3f5503c6d74
yuichiroTCY <t.yic.yt@gmail.com>
2016-09-22 02:05:25 +0900

    (refs #247) Update MultiInput to add new input without explict clicking on add button

--- src/web/forms/fields/__tests__/MultiInput-test.js
+++ src/web/forms/fields/__tests__/MultiInput-test.js
@@ -3,14 +3,14 @@
 jest.unmock('react-redux');
 jest.unmock('../MultiInput');
 
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import React from 'react';
 
 import { MultiInputComponent } from '../MultiInput';
 
 describe('<MultiInputComponent />', () => {
   it('shows <input />s according to given values', () => {
-    const wrapper = shallow(<MultiInputComponent
+    const wrapper = mount(<MultiInputComponent
       label="THISISMULTIINPUT"
       values={['AAAA', 'BBBB', 'CCCC']}
     />);
@@ -25,7 +25,7 @@ describe('<MultiInputComponent />', () => {
   });
 
   it('does not show any <input />s if no value given', () => {
-    const wrapper = shallow(<MultiInputComponent
+    const wrapper = mount(<MultiInputComponent
       label="THISISMULTIINPUT"
       values={null}
     />);
@@ -37,7 +37,7 @@ describe('<MultiInputComponent />', () => {
   });
 
   it('contains <input />s whose type is specified by prop', () => {
-    const wrapper = shallow(<MultiInputComponent
+    const wrapper = mount(<MultiInputComponent
       values={['AAAA', 'BBBB']}
       type="number"
     />);
@@ -45,5 +45,22 @@ describe('<MultiInputComponent />', () => {
     const inputs = wrapper.find('input');
     expect(inputs.at(0).prop('type')).toEqual('number');
     expect(inputs.at(1).prop('type')).toEqual('number');
+  });
+
+  it('adds new input just by typing', () => {
+    const onChange = jest.fn();
+    const onAddItemRequested = jest.fn();
+
+    const wrapper = mount(<MultiInputComponent
+      values={['AAAA', 'BBBB']}
+      onChange={onChange}
+      onAddItemRequested={onAddItemRequested}
+    />);
+
+    const inputs = wrapper.find('input');
+    const targetInput = inputs.at(2);
+
+    targetInput.simulate('change', { target: { value: 'a' } });
+    expect(onAddItemRequested).toBeCalledWith('a');
   });
 });
