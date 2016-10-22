d0e5dabcb1770d025b4d3ff7ff4b622e658669c0
yuichiroTCY <t.yic.yt@gmail.com>
2016-10-22 15:21:45 +0900

    (refs #322) Add ICD10 code for Dhulikhel hospital

--- src/data/index.js
+++ src/data/index.js
@@ -2,8 +2,10 @@ import sortBy from 'lodash.sortby';
 import uniqBy from 'lodash.uniqby';
 
 import _ICD10 from '../../assets/data/icd10.json';
+import _dhulikhelICD10 from '../../assets/data/dhulikhel-icd10.json';
 
-export const ICD10 = sortBy(uniqBy(_ICD10, 'code'), 'code').map(item => ({
-  ...item,
-  _query: ` ${item.code} ${item.description}`.toLowerCase(),  // suggestion検索用 / 前方一致用にスペース入れる
-}));
+export const ICD10 = sortBy(uniqBy(_ICD10.concat(_dhulikhelICD10), 'code'), 'code')
+  .map(item => ({
+    ...item,
+    _query: ` ${item.code} ${item.description}`.toLowerCase(),  // suggestion検索用 / 前方一致用にスペース入れる
+  }));
