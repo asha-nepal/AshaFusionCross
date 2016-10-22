030bc142820f8aa986be6382549be2a3f2e95c79
yuichiroTCY <t.yic.yt@gmail.com>
2016-10-22 15:01:53 +0900

    (refs #322) Update ICD10 data

--- src/data/index.js
+++ src/data/index.js
@@ -1,6 +1,9 @@
+import sortBy from 'lodash.sortby';
+import uniqBy from 'lodash.uniqby';
+
 import _ICD10 from '../../assets/data/icd10.json';
-export const ICD10 = Object.keys(_ICD10).sort().map(code => ({
-  code,
-  description: _ICD10[code],
-  _query: ` ${code} ${_ICD10[code]}`.toLowerCase(),  // suggestion検索用 / 前方一致用にスペース入れる
+
+export const ICD10 = sortBy(uniqBy(_ICD10, 'code'), 'code').map(item => ({
+  ...item,
+  _query: ` ${item.code} ${item.description}`.toLowerCase(),  // suggestion検索用 / 前方一致用にスペース入れる
 }));
