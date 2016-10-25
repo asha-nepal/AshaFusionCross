1989b26150ca6be8a159641ddf352dbc056732e7
yuichiroTCY <t.yic.yt@gmail.com>
2016-10-26 01:51:57 +0900

    (refs #258) Fix SubformList to take Array<string> as value to migrate from MultiInput

--- src/web/forms/fields/SubformList.js
+++ src/web/forms/fields/SubformList.js
@@ -18,6 +18,7 @@ type FormFieldDefinition = {
   field: string,
   class: string,
   label?: string,
+  primary?: boolean,
 }
 
 const RowComponent = ({
@@ -26,54 +27,66 @@ const RowComponent = ({
   onChange,
   onRemoveItemRequested,
 }: {
-  value: Object,
+  value: Object | string,
   fields: Array<FormFieldDefinition>,
   onChange: (newValue: Object) => void,
   onRemoveItemRequested?: () => void,
-}) => (
-  <div className="panel-block">
-    <div className="columns">
-      <div className="column">
-        <div className="control is-grouped">
-        {fields.map((field, i) => {
-          const component = fieldComponents[field.class];
+}) => {
+  let _value = {};
+  if (typeof value === 'string') {
+    // MultiInputからの移行．primary=trueのfieldに当てはめる
+    const primaryField = fields.find(f => f.primary);
+    if (primaryField) {
+      _value[primaryField.field] = value;
+    }
+  } else {
+    _value = value;
+  }
 
-          return React.createElement(component, {
-            key: i,
-            label: field.label,
-            value: value[field.field],
-            onChange: (v => {
-              const updated = {};
-              updated[field.field] = v;
+  return (
+    <div className="panel-block">
+      <div className="columns">
+        <div className="column">
+          <div className="control is-grouped">
+          {fields.map((field, i) => {
+            const component = fieldComponents[field.class];
 
-              onChange({
-                ...value,
-                ...updated,
-              });
-            }),
-          });
-        })}
+            return React.createElement(component, {
+              key: i,
+              label: field.label,
+              value: _value[field.field],
+              onChange: (v => {
+                const updated = {};
+                updated[field.field] = v;
+
+                onChange({
+                  ..._value,
+                  ...updated,
+                });
+              }),
+            });
+          })}
+          </div>
         </div>
+
+        {onRemoveItemRequested &&
+          <div style={{ position: 'relative' }}>
+            <a
+              style={{ height: '100%' }}
+              className="button is-danger"
+              onClick={e => {
+                e.preventDefault();
+                if (onRemoveItemRequested) onRemoveItemRequested();
+              }}
+            >
+              <i className="fa fa-times" />
+            </a>
+          </div>
+        }
       </div>
-
-      {onRemoveItemRequested &&
-        <div style={{ position: 'relative' }}>
-          <a
-            style={{ height: '100%' }}
-            className="button is-danger"
-            onClick={e => {
-              e.preventDefault();
-              if (onRemoveItemRequested) onRemoveItemRequested();
-            }}
-          >
-            <i className="fa fa-times" />
-          </a>
-        </div>
-      }
     </div>
-
-  </div>
-);
+  );
+};
 
 export const SubformListComponent = ({
   label,
@@ -84,7 +97,7 @@ export const SubformListComponent = ({
   onRemoveItemRequested,
 }: {
   label?: ?string,
-  values: ?Array<Object>,
+  values: ?Array<Object | string>,
   fields: Array<FormFieldDefinition>,
   onChange: (index: number, newValue: Object) => void,
   onAddItemRequested: (value: Object) => void,
