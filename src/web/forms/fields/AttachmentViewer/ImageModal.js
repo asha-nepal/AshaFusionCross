6475b405b37f0c0774a997fe0c362622380c28dc
yuichiroTCY <t.yic.yt@gmail.com>
2017-03-04 00:17:40 +0900

    (refs #426) Fix ImageModal and ImageThumbnail to check blob equality to update

--- src/web/forms/fields/AttachmentViewer/ImageModal.js
+++ src/web/forms/fields/AttachmentViewer/ImageModal.js
@@ -1,60 +1,77 @@
 /* @flow */
 
-import React from 'react';
+import React, { Component } from 'react';
 import Modal from '../../../components/Modal';
 import downloadBlob from 'lib/download-blob';
 
-export default ({
-  isOpen,
-  onClose,
-  imageBlob,
-  imageName,
-}: {
+type Props = {
   isOpen: boolean,
   onClose: () => void,
   imageBlob: ?Blob,
   imageName: ?string,
-}) => {
-  if (!imageBlob) return null;
+}
 
-  let imageUrl;
-  try {
-    imageUrl = URL.createObjectURL(imageBlob);
-  } catch (e) {
-    return null;
+export default class extends Component {
+  shouldComponentUpdate(nextProps: Props) {
+    if (nextProps.isOpen !== this.props.isOpen) return true;
+    if (nextProps.imageBlob !== this.props.imageBlob) return true;
+    if (nextProps.imageName !== this.props.imageName) return true;
+    // Skip checking equality of onClose
+
+    return false;
   }
 
-  return (
-    <Modal
-      isOpen={isOpen}
-      onClose={onClose}
-    >
-      {imageUrl &&
-        <p className="image" style={{ marginBottom: 20 }}>
-          <img
-            src={imageUrl}
-            alt={imageName || ''}
-          />
-        </p>
-      }
-      <div className="has-text-centered">
-        {imageBlob &&
-          <a
-            className="icon is-medium inverse"
-            onClick={e => {
-              e.preventDefault();
-              if (imageBlob) downloadBlob(imageBlob, imageName || 'file');
-            }}
-          ><i className="fa fa-download" /></a>
-        }
+  props: Props
+
+  render() {
+    const {
+      isOpen,
+      onClose,
+      imageBlob,
+      imageName,
+    } = this.props;
+
+    if (!imageBlob) return null;
+
+    let imageUrl;
+    try {
+      imageUrl = URL.createObjectURL(imageBlob);
+    } catch (e) {
+      return null;
+    }
+
+    return (
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+      >
         {imageUrl &&
-          <a
-            className="icon is-medium inverse"
-            href={imageUrl}
-            target="_blank"
-          ><i className="fa fa-external-link" /></a>
+          <p className="image" style={{ marginBottom: 20 }}>
+            <img
+              src={imageUrl}
+              alt={imageName || ''}
+            />
+          </p>
         }
-      </div>
-    </Modal>
-  );
-};
+        <div className="has-text-centered">
+          {imageBlob &&
+            <a
+              className="icon is-medium inverse"
+              onClick={e => {
+                e.preventDefault();
+                if (imageBlob) downloadBlob(imageBlob, imageName || 'file');
+              }}
+            ><i className="fa fa-download" /></a>
+          }
+          {imageUrl &&
+            <a
+              className="icon is-medium inverse"
+              href={imageUrl}
+              target="_blank"
+            ><i className="fa fa-external-link" /></a>
+          }
+        </div>
+      </Modal>
+    );
+  }
+}
