6d2ffaa504aa9dd7454d8846f51e079ed4ec5310
yuichiroTCY <t.yic.yt@gmail.com>
2017-04-23 18:55:29 +0900

    (refs #445) Install react-native-randombytes and replace randomstring with randomstring-promise

--- android/app/src/main/java/com/ashafusioncross/MainApplication.java
+++ android/app/src/main/java/com/ashafusioncross/MainApplication.java
@@ -3,6 +3,7 @@ package com.ashafusioncross;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.bitgo.randombytes.RandomBytesPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RandomBytesPackage(),
             new VectorIconsPackage(),
             new ReactMaterialKitPackage()
       );
