61783bb8259095b43c70e1a7df24923af1c35012
yuichiroTCY <t.yic.yt@gmail.com>
2017-05-17 00:15:23 +0900

    (refs #445) Add icons for all platforms

--- utils/create-icons.sh
+++ utils/create-icons.sh
@@ -3,17 +3,51 @@
 CWD=`dirname "${0}"`
 APP_ROOT="`cd "${CWD}/../";pwd`"
 SRC=$APP_ROOT/assets/img/logo.svg
-DST_DIR=$APP_ROOT/assets/icons/AshaFusionCross.iconset/
-sizes=(16 32 128 256 512 1024)
+DST=$APP_ROOT/assets/icons
 
-mkdir -p $DST_DIR
-for i in "${sizes[@]}"
-do
-    SIZE=${i}x${i}
-    SIZE2X=$((i*2))x$((i*2))
-    convert -background none -size $SIZE -extent $SIZE -gravity center $SRC $DST_DIR/icon_$SIZE.png
-    convert -background none -size $SIZE2X -extent $SIZE2X -gravity center $SRC $DST_DIR/icon_$SIZE@2x.png
-done
+convert_icon() {
+    local size=$1
+    local src=$2
+    local dst=$3
 
-iconutil -c icns $DST_DIR
-rm -rf $DST_DIR
+    convert -background none -size $size -extent $size -gravity center $src $dst
+}
+
+create_mac() {
+    local DST_DIR=$DST/mac/asha.iconset/
+    local sizes=(16 32 128 256 512 1024)
+
+    mkdir -p $DST_DIR
+    for i in "${sizes[@]}"
+    do
+        local SIZE=${i}x${i}
+        local SIZE2X=$((i*2))x$((i*2))
+        convert_icon $SIZE $SRC $DST_DIR/icon_$SIZE.png
+        convert_icon $SIZE2X $SRC $DST_DIR/icon_$SIZE@2x.png
+    done
+
+    iconutil -c icns $DST_DIR
+    rm -rf $DST_DIR
+}
+
+create_win() {
+    local DST_DIR=$DST/win
+    local DST_FILE=$DST_DIR/asha.ico
+    local SIZE="256x256"
+
+    mkdir -p $DST_DIR
+    convert_icon $SIZE $SRC $DST_FILE
+}
+
+create_png() {
+    local DST_DIR=$DST/png
+    local SIZE="1024x1024"
+    local DST_FILE=$DST_DIR/1024x1024.png
+
+    mkdir -p $DST_DIR
+    convert_icon $SIZE $SRC $DST_FILE
+}
+
+create_win
+create_mac
+create_png
