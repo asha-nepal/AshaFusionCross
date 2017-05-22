a533b11e49051bb7b002f6d7b85367c7cf9ba86f
tuttieee <t.yic.yt@gmail.com>
2017-05-22 10:36:41 +0900

    Fix alertss-saga not to use randomstring-promise

--- src/sagas/alerts.js
+++ src/sagas/alerts.js
@@ -1,14 +1,18 @@
 import { delay } from 'redux-saga';
 import { take, put, call, fork } from 'redux-saga/effects';
-import randomstringPromise from 'randomstring';
 import {
   PUSH_ALERT,
   addAlert,
   removeAlert,
 } from '../actions';
 
+let _id = 0;
+function getId() {
+  return _id++;
+}
+
 export function* pushAlert(message: string, type: string, timeout: number = 2000) {
-  const id = yield call(randomstringPromise);
+  const id = getId();
   yield put(addAlert(id, message, type));
   yield call(delay, timeout);
   yield put(removeAlert(id));
