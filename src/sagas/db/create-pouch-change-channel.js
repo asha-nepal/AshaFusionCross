f0116f2d90a8822b37636b14b4b11bfa889eb1ff
yuichiroTCY <t.yic.yt@gmail.com>
2017-07-18 01:00:54 +0900

    (refs #483) Fix createPouchChangeChannel to retry db.changes() when ETIMEDOUT occurs

--- src/sagas/db/create-pouch-change-channel.js
+++ src/sagas/db/create-pouch-change-channel.js
@@ -3,39 +3,54 @@ import { eventChannel, END } from 'redux-saga';
 
 export default function (db: PouchInstance) {
   return eventChannel(emit => {
-    const feed = db.changes({
-      since: 'now',
-      live: true,
-      include_docs: true,
-    })
-    .on('change', change => {
-      // handle change
-      emit({
-        type: 'change',
-        payload: {
-          change,
-        },
+    let feed;
+
+    function startChangesFeed() {
+      feed = db.changes({
+        since: 'now',
+        live: true,
+        include_docs: true,
+      })
+      .on('change', change => {
+        // handle change
+        emit({
+          type: 'change',
+          payload: {
+            change,
+          },
+        });
+      })
+      .on('complete', info => {
+        // changes() was canceled
+        emit({
+          type: 'complete',
+          payload: {
+            info,
+          },
+        });
+        feed.cancel();
+        emit(END);
+      })
+      .on('error', error => {
+        if (error.code === 'ETIMEDOUT') {
+          // Retry if timeout error occurs because some proxy intercepts heartbeat signals
+          // and it is able to start listening changes again in such case.
+          console.log('ETIMEDOUT');
+          feed.cancel();
+          startChangesFeed();
+          return;
+        }
+
+        emit({
+          type: 'error',
+          error,
+        });
+        feed.cancel();
+        emit(END);
       });
-    })
-    .on('complete', info => {
-      // changes() was canceled
-      emit({
-        type: 'complete',
-        payload: {
-          info,
-        },
-      });
-      feed.cancel();
-      emit(END);
-    })
-    .on('error', error => {
-      emit({
-        type: 'error',
-        error,
-      });
-      feed.cancel();
-      emit(END);
-    });
+    }
+
+    startChangesFeed();
 
     const unsubscribe = () => feed.cancel();
 
