249b8aaa9f36ea6ac21f62bd62894b5055375a82
yuichiroTCY <t.yic.yt@gmail.com>
2017-07-19 00:15:53 +0900

    (refs #483) Add test for watchOnPouchChanges

--- src/sagas/db/__tests__/index-test.js
+++ src/sagas/db/__tests__/index-test.js
@@ -1,10 +1,15 @@
 /* eslint-env jest, jasmine */
 
+jest.unmock('redux-saga/effects');
 jest.unmock('../index');
+jest.mock('../handle-changes', () => jest.fn());
 
+import { call } from 'redux-saga/effects';
 import {
   formatHostname,
+  watchOnPouchChanges,
 } from '../index';
+import handlePouchChanges from '../handle-changes';
 
 describe('formatHostname', () => {
   it('does nothing if scheme provided', () => {
@@ -30,5 +35,25 @@ describe('formatHostname', () => {
       .toEqual('http://example.com');
     expect(formatHostname('http://example.com:6789/'))
       .toEqual('http://example.com:6789');
+  });
+});
+
+describe('watchOnPouchChanges', () => {
+  it('updates patient or record data when changes event emitted', () => {
+    const db = jest.fn();
+
+    const saga = watchOnPouchChanges(db);
+
+    const channelRetval = {
+      type: 'change',
+      payload: {
+        change: { foo: 'bar' },
+      },
+    };
+
+    saga.next();
+    saga.next(jest.fn());
+    expect(saga.next(channelRetval).value)
+      .toEqual(call(handlePouchChanges, { foo: 'bar' }));
   });
 });
