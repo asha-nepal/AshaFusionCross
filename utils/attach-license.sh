1a15329f59a721f605f8a408f130c1815b2c6ec0
tuttieee <t.yic.yt@gmail.com>
2017-08-12 03:32:31 +0900

    (refs #515) Add ASLv2 descriptions to js files under utils

--- utils/attach-license.sh
+++ utils/attach-license.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+#
+# Copyright 2017 Yuichiro Tsuchiya
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 # Usage:
 # $ find src -name "*.js" | xargs -I{} ./utils/attach-license.sh {} contributors.csv
 
@@ -85,4 +100,4 @@ IFS=
 LICENSEDESC=`getASLDescription $1 $2`
 LICENSECOMMENT=`convertToJSComment $LICENSEDESC`
 
-echo $(echo -e ${LICENSECOMMENT}"\n" | cat - $1) > $1
+echo $(echo -e ${LICENSECOMMENT}"\n" | cat - $1)
