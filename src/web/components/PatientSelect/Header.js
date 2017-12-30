94943c3d19c7b88acc0548b41887c8115c3b4717
tuttieee <t.yic.yt@gmail.com>
2017-12-30 16:05:07 +0900

    (refs #495) Fix PatientSelect/Header to be compatible with bulma v0.6

--- src/web/components/PatientSelect/Header.js
+++ src/web/components/PatientSelect/Header.js
@@ -18,6 +18,8 @@
 
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import classNames from 'classnames';
+import logo from '../../../../assets/img/logo-white.svg';
 
 type Props = {
   isLoggedIn: boolean,
@@ -50,44 +52,44 @@ export default class extends Component {
     } = this.props;
 
     return (
-      <section className="hero is-primary is-bold">
-        <div className="hero-head">
-          <div className="container">
-            <nav className="nav">
-              <div className="nav-left">
-                <span className="nav-item is-brand title">ASHA fusion</span>
-              </div>
+      <nav className="navbar is-primary">
+        <div className="container">
+          <div className="navbar-brand">
+            <div className="navbar-item">
+              <img src={logo} alt="ASHA fusion" />
+            </div>
+            <div
+              className={classNames('navbar-burger', { 'is-active': this.state.isMenuOpen })}
+              onClick={e => {
+                e.preventDefault();
+                this.setState({ isMenuOpen: !this.state.isMenuOpen });
+              }}
+            >
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          </div>
 
-              <span
-                className="nav-toggle"
-                onClick={e => {
-                  e.preventDefault();
-                  this.setState({ isMenuOpen: !this.state.isMenuOpen });
-                }}
-              >
-                <span></span>
-                <span></span>
-                <span></span>
-              </span>
-              {isLoggedIn &&
-                <div className={`nav-right nav-menu${this.state.isMenuOpen ? ' is-active' : ''}`}>
-                  <p className="nav-item">{loggedInUser || '(anonymous user)'}</p>
-                  {isAdmin && <Link className="nav-item" to="admin">Admin</Link>}
-                  {logout &&
-                    <a
-                      className="nav-item"
-                      onClick={e => {
-                        e.preventDefault();
-                        logout();
-                      }}
-                    >Log out</a>
-                  }
-                </div>
-              }
-            </nav>
+          <div className={classNames('navbar-menu', { 'is-active': this.state.isMenuOpen })}>
+            {isLoggedIn &&
+              <div className="navbar-end">
+                <div className="navbar-item">{loggedInUser || '(anonymous user)'}</div>
+                {isAdmin && <Link className="navbar-item" to="admin">Admin</Link>}
+                {logout &&
+                  <a
+                    className="navbar-item"
+                    onClick={e => {
+                      e.preventDefault();
+                      logout();
+                    }}
+                  >Log out</a>
+                }
+              </div>
+            }
           </div>
         </div>
-      </section>
+      </nav>
     );
   }
 }
