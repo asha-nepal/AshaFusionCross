b43977c34e47c7bd616206f87466c92f93aff01e
tuttieee <t.yic.yt@gmail.com>
2018-01-07 14:49:51 +0900

    (refs #537) Fix tests

---
+++ src/web/forms/editor/__mocks__/DraggableFieldEditor.js
@@ -0,0 +1,10 @@
+/* @flow */
+
+import React from 'react';
+
+export default ({ children }: {children: React$Element<any>}) => (
+  <div>
+    <p>DraggableFieldEditor</p>
+    {children}
+  </div>
+);
