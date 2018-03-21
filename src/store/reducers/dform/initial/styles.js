485b0caf4fb56718fe3ea525d44eccc56afe23fd
tuttieee <t.yic.yt@gmail.com>
2018-03-21 14:50:04 +0900

    Add medicine brandnames list for pharping

--- src/store/reducers/dform/initial/styles.js
+++ src/store/reducers/dform/initial/styles.js
@@ -1,3 +1,22 @@
+/**
+ * Copyright 2017 Yuichiro Tsuchiya
+ * Copyright 2017 Yuguan Xing
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import medicines from '../../../../../data/pharping-camp-medicine-list-brandnames.json';
+
 export default {
   patient: [
     {
@@ -274,7 +293,7 @@ export default {
               fields: [
                 {
                   field: 'medicine', label: 'Medicine',
-                  class: 'textinput', primary: true, suggestions: [],
+                  class: 'textinput', primary: true, suggestions: medicines,
                 },
                 {
                   field: 'stat',
