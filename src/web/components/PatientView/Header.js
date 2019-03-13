f08cf54ec36ee0c87860055f0b58997b8ad0871d
Yuichiro Tsuchiya <t.yic.yt@gmail.com>
2019-03-13 23:11:51 +0900

    Fix PatietView/Header

--- src/web/components/PatientView/Header.js
+++ src/web/components/PatientView/Header.js
@@ -59,8 +59,8 @@ export default ({
         key="name"
         className="navbar-item is-hidden-touch"
       >
-        {`${patient.number ? `[${patient.number}] ` : ''}
-        + ${patient.first_name} ${patient.last_name}`}
+        {`${patient.number ? `[${patient.number}] ` : ''}`}
+        {`${patient.first_name} ${patient.last_name}`}
       </span>,
     ]}
     menu={
